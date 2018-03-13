896fecd2125b8a3efc34d0c2e36a50d8ee61347b
Justin Joseph <justin.joseph72@outlook.com>
2018-03-13 16:34:36 +0000

    adding

---
+++ content-cloud-app/src/main/java/com/yoti/app/ContentCloudAppliction.java
@@ -0,0 +1,11 @@
+package com.yoti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCloudAppliction {
+    public static void main(String[] args){
+        SpringApplication.run(ContentCloudAppliction.class,args);
+    }
+}
