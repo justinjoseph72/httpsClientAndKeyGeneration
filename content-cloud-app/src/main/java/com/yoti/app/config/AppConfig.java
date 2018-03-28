a11f6a2cfab990b319a39c04bf364adc4990157b
Justin Joseph <justin.joseph72@outlook.com>
2018-03-28 10:48:43 +0100

    adding BC security provider

--- content-cloud-app/src/main/java/com/yoti/app/config/AppConfig.java
+++ content-cloud-app/src/main/java/com/yoti/app/config/AppConfig.java
@@ -1,21 +1,31 @@
 package com.yoti.app.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 
 @Configuration
+@Slf4j
 public class AppConfig {
+
+    @PostConstruct
+    public void init() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     @Bean
     public HttpComponentsClientHttpRequestFactory getHttpComponentsClientHttpRequestFactory() {
@@ -27,12 +37,8 @@ public class AppConfig {
                     .setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
             requestFactory = new HttpComponentsClientHttpRequestFactory();
             requestFactory.setHttpClient(httpClient);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            log.warn("Exception in in initalizing rest template {} {}", e.getClass().getName(), e.getMessage());
         }
         return requestFactory;
     }
